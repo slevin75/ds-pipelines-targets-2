b2bb2a1a2851f50e07ef51c0424e144639fccb56
Levin <slevin@usgs.gov>
2021-10-13 12:40:41 -0500

    made changes to fetch and process functions and targets.R

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -9,11 +9,7 @@ annotate_data <- function(site_data_clean, site_filename){
   site_info <- read_csv(site_filename)
   annotated_data <- left_join(site_data_clean, site_info, by = "site_no") %>% 
     select(station_name = station_nm, site_no, dateTime, water_temperature, latitude = dec_lat_va, longitude = dec_long_va)
-  
+  mutate(annotated_data,station_name=as.factor(station_name))
   return(annotated_data)
 }
 
-
-style_data <- function(site_data_annotated){
-  mutate(site_data_annotated, station_name = as.factor(station_name))
-}
--- _targets.R
+++ _targets.R
@@ -1,15 +1,50 @@
 library(targets)
+library(readr)
+
 source("1_fetch/src/get_nwis_data.R")
 source("2_process/src/process_and_style.R")
 source("3_visualize/src/plot_timeseries.R")
 
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse because we need dplyr, ggplot2, readr, stringr, and purrr
+p_width <- 12
+p_height <- 7
+p_units <- "in"
+startDate<-"2014-05-01"
+endDate<-"2015-05-01"
+parameterCd<-'00010'
 
+
+###download, save, and compile data for each site
 p1_targets_list <- list(
   tar_target(
+    g01427207,
+    download_nwis_site_data(site_num= "01427207",outdir="1_fetch/out",parameterCd,startDate,endDate), 
+    format="file"
+  ),
+  tar_target(
+    g01432160,
+    download_nwis_site_data(site_num= "01432160",outdir="1_fetch/out",parameterCd,startDate,endDate), 
+    format="file"
+  ),
+  tar_target(
+    g01436690,
+    download_nwis_site_data(site_num= "01436690",outdir="1_fetch/out",parameterCd,startDate,endDate), 
+    format="file"
+  ),
+  tar_target(
+    g01466500,
+    download_nwis_site_data(site_num= "01466500",outdir="1_fetch/out",parameterCd,startDate,endDate), 
+    format="file"
+  ),
+  tar_target(
+    g01435000,
+    download_nwis_site_data(site_num= "01435000",outdir="1_fetch/out",parameterCd,startDate,endDate), 
+    format="file"
+  ),
+  tar_target(
     site_data,
-    download_nwis_data(),
+    compile_nwis_data(c(g01427207, g01432160,g01436690, g01466500,  g01435000)),
   ),
   tar_target(
     site_info_csv,
@@ -17,6 +52,8 @@ p1_targets_list <- list(
     format = "file"
   )
 )
+
+
 
 p2_targets_list <- list(
   tar_target(
@@ -26,17 +63,14 @@ p2_targets_list <- list(
   tar_target(
     site_data_annotated,
     annotate_data(site_data_clean, site_filename = site_info_csv)
-  ),
-  tar_target(
-    site_data_styled,
-    style_data(site_data_annotated)
   )
 )
 
 p3_targets_list <- list(
   tar_target(
     figure_1_png,
-    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_styled),
+    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_annotated,
+                         width=p_width,height=p_height,units=p_units),
     format = "file"
   )
 )
