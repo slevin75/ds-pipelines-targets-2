5474222c4de5d7d3643bc73b6399fe720b4aac51
Levin <slevin@usgs.gov>
2021-10-18 11:32:35 -0500

    updated target names to include p1_ ect

--- 2_process/src/2_process.R
+++ 2_process/src/2_process.R
@@ -5,12 +5,12 @@ source("2_process/src/process_and_style.R")
 
 p2_targets_list <- list(
   tar_target(
-    site_data_clean, 
-    process_data(site_data)
+    p2_site_data_clean, 
+    process_data(p1_site_data)
   ),
   tar_target(
-    site_data_annotated_csv,
-    annotate_data(site_data_clean, site_filename = site_info_csv, fileout="2_process/out/site_data_annotated.csv"),
+    p2_site_data_annotated_csv,
+    annotate_data(p2_site_data_clean, site_filename = p1_site_info_csv, fileout="2_process/out/site_data_annotated.csv"),
     format= "file"
   )
 )
--- 3_visualize/src/3_visualize.R
+++ 3_visualize/src/3_visualize.R
@@ -5,8 +5,8 @@ source("3_visualize/src/plot_timeseries.R")
 
 p3_targets_list <- list(
   tar_target(
-    figure_1_png,
-    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_annotated_csv,
+    p3_figure_1_png,
+    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", p2_site_data_annotated_csv,
                          width=p_width,height=p_height,units=p_units),
     format = "file"
   )
